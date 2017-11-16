ab3e4dcc13472619a72270ad964735e076416dbf
Rubina Pradhan <rubinapradhan@Rubinas-MBP.home>
2017-11-16 16:50:01 -0500

    test

---
+++ index.js
@@ -0,0 +1,26 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config');
+
+// DB Setup
+mongoose.connect('mongodb://config.mongoUser:config.mongoPass@ds257495.mlab.com:57495/coolgifts');
+// mongoose.connect('mongodb://localhost:auth/auth');
+
+// App Setup
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
