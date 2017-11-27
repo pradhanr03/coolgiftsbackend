c8130049a7b48346abe3e61c40769648c3bc2ac7
Rubina Pradhan <rubinapradhan@Rubinas-MBP.home>
2017-11-27 17:49:33 -0500

    updating server

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 3090;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
